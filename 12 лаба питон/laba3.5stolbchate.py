f7b5a8cc426d5ec9a7818844e904592d82d6f261
Maxim999-a <70699430+Maxim999-a@users.noreply.github.com>
2022-04-26 16:30:53 +0300

    Delete 12 лаба питон directory

--- 12 лаба питон/laba3.5stolbchate.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#Пример построения точечного графика с помощью функции scatter() с изменение цвета 
-
-import numpy as np
-import matplotlib.pyplot as plt
-np.random.seed(123)
-groups = [f"P{i}" for i in range(7)]
-counts = np.random.randint(3, 10, len(groups))
-plt.bar(groups, counts)
